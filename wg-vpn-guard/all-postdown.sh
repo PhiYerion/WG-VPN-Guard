104ccdd6ce5ada7472ddec3820d57ff05f7aa1cd
phiyerion <reidbandy@gmail.com>
2023-11-20 21:48:30 -0500

    excessive comments

--- wg-vpn-guard/all-postdown.sh
+++ wg-vpn-guard/all-postdown.sh
@@ -1,20 +1,35 @@
-ip6tables -A OUTPUT -j ACCEPT
-ip6tables -A INPUT -j ACCEPT
+#!/bin/sh
+
+# Reset ipv6
+## system
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.default.disable_ipv6=0
 sysctl -w net.ipv6.conf.lo.disable_ipv6=0
 
+## iptables
+ip6tables -A OUTPUT -j ACCEPT
+ip6tables -A INPUT -j ACCEPT
+
+# provide fault tolerance
 while true; do
+	# flush rules
 	ip6tables -F WG3_V6_DROP
 	iptables  -F OUTPUT_WG3_V4
 	iptables  -F INPUT_WG3_V4
+
+	# There are problems with deleting the chains if there
+	# are references to them.
+	## delete references to chains
 	iptables  -D INPUT  -j INPUT_WG3_V4
 	iptables  -D OUTPUT -j OUTPUT_WG3_V4
 	ip6tables -D OUTPUT -j WG3_V6_DROP
 	ip6tables -D INPUT  -j WG3_V6_DROP
+	## allow time for the deletion to take effect
 	sleep 0.5
+	## delete chains and repeat the loop until successful
 	ip6tables -X WG3_V6_DROP   || continue
 	iptables  -X OUTPUT_WG3_V4 || continue
 	iptables  -X INPUT_WG3_V4  || continue
+	## If deleting the chains didn't error, continue
 	break
 done
--- wg-vpn-guard/gen_gaurd.sh
+++ wg-vpn-guard/gen_gaurd.sh
@@ -1,27 +1,49 @@
-#!/bin/bash
+#!/bin/sh
 current_dir="$(cd "$(dirname "$0")" && pwd)"
+
+# This file is used to reset iptables and sysctl after the vpn
+# is disconnected. The postdown file will be the same for all vpns.
 base_postdown_file="$current_dir/all-postdown.sh"
+
+# This includes iptables and sysctl config to disable ipv6 and block
+# everything that is not going through the VPN. The postup file will
+# be modified for each VPN.
 base_postup_file="$current_dir/postup.frame"
+
+# Location for the postdown file. The location will also be the same
+# for all vpns.
 postdown_file='/etc/wireguard/all-postdown.sh'
 
+# Make sure the base postdown file exists
 if [ ! -f "$base_postdown_file" ]; then
 	echo "ERROR: $base_postdown_file not found"
 	exit 1
 fi
+
+# Make sure the base postup file exists
 if [ ! -f "$base_postup_file" ]; then
 	echo "ERROR: $base_postup_file not found"
 	exit 1
 fi
 
+# If the postdown file we would be writting to exists, proceed. Otherwise
+# leave it.
 if [ ! -f "$postdown_file" ]; then
 	echo "Creating $postdown_file"
+	# WG-VPN-Guard/all-postdown.sh -> /etc/wireguard/all-postdown.sh
 	cp "$base_postdown_file" "$postdown_file" || exit 1
+	# Make sure the file is executable
 	chmod +x "$postdown_file" || exit 1
 fi
 
+# For each vpn config file, create a postup file and line the config
+# to the postup and postdown files (at connection and at disconnection,
+# respectively).
 for file in /etc/wireguard/wg*.conf; do
+	# e.g. /etc/wireguard/wg0.conf-postup.sh
 	postup_file="$file-postup.sh"
 
+	# If the postup file exists, delete it and create a new one.
 	if [ -f "$postup_file" ]; then
 		echo "Replacing $postup_file"
 		rm "$postup_file" || exit 1
@@ -29,22 +51,39 @@ for file in /etc/wireguard/wg*.conf; do
 		echo "Creating $postup_file"
 	fi
 
+	# Get the address and port from the config file
 	address=$(grep -oP '(?<=Endpoint = )[0-9.]*' "$file")
 	port=$(grep -oP '(?<=:)[0-9]*' "$file")
+
+	# Create the postup file
+	## Sha-bang to the shell
 	printf '#!/bin/sh\n' > "$postup_file" || exit 1
+	## Create the proper variables for the postup file
+	## to reference
 	printf 'VPN_SERVER=%s\nPORT=%s\n' \
 		   "$address" "$port" \
 		   >> "$postup_file" || exit 1
+	## Add the base postup file to the postup file
 	cat "$base_postup_file" >> "$postup_file" || exit 1
 
+	## Make sure the file is executable
 	chmod +x "$postup_file" || exit 1
 
+	# Make the wg<#>.conf file run the postup file at connection
+	# and postdown at disconnection.
+	## Create postup and postdown lines for the config
+	## file (e.g /etc/wireguard/wg0.conf)
 	postup_line="Postup = $postup_file" 
 	postdown_line="Postdown = $postdown_file"
+
+	## If the postup or postdown lines are not in the config
+	## file, add them.
 	if ! grep -q "$postup_line" "$file"; then
+		## Add the postup line after the [Interface] line
 		sed -i "/\[Interface\]/a $postup_line" "$file" || exit 1
 	fi
 	if ! grep -q "$postdown_line" "$file"; then
+		## Add the postdown line after the [Interface] line
 		sed -i "/\[Interface\]/a $postdown_line" "$file" || exit 1
 	fi
 done
